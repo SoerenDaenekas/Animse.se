3ffdfd3caac319d744c66bd2493b5ab977c5c850
Sören Dänekas <daenekassoeren@gmail.com>
2021-08-02 20:45:44 +0200

    Load from rest api

--- src/Events.js
+++ src/Events.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './Events.css';
 
 function SingleEvent(props) {
@@ -6,43 +7,61 @@ function SingleEvent(props) {
     return (
         <div className="SingleEvent row">
             <div className="Countdown col-md-3">
-                imagine an image here
+                <img className="img-fluid" src={"http://api.animse.se:8055/assets/" + eventInfo.banner}/>
             </div>
             <div className="Info col-md-7">
                 <div className="row">
                     <div className="Title col-auto">{eventInfo.title}</div>
-                    <div className="Date col">{eventInfo.startDate} - {eventInfo.endDate}</div>
+                    <div className="Date col">{eventInfo.startdate} - {eventInfo.enddate}</div>
+                    <div>{eventInfo.shortdescription}</div>
                 </div>
             </div>
             <div className="Location col-md-2">
-                {eventInfo.place}
+                {eventInfo.location}
             </div>
         </div>
     );
 }
 
-function Events(props) {
-    var events = [
-        {
-            title: "ACCS Möte", 
-            place: "Awoogatan 69, 99966 Stockholm, Sverige",
-            startDate: "16.10.1993",
-            endDate: "12.11.1996",
-        },
-    ];
-    for (let index = 0; index < 30; index++) {
-        events.push(events[0])
+class Events extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            events: [],
+            status: 'loading'
+        }
     }
-    events.push(events[0])
 
-    return (
-        <div className="Events container"> 
-        {
-            events.map(e => <SingleEvent eventInfo={e} />)
-        }            
-        </div>
-    );
+    componentDidMount() {
+        fetch("http://api.animse.se:8055/items/animeevents/")
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({
+                events: data.data,
+                status: 'idle'
+            })
+        })        
+    }
+
+    render() {
+        if (this.state.status == 'loading') {
+            return (
+                <div>
+                    loading....
+                </div>
+            )
+        }
+
+    
+        return (
+            <div className="Events container"> 
+            {
+                this.state.events.map(e => <SingleEvent eventInfo={e} />)
+            }            
+            </div>
+        );
+    }
 }
- 
+
 
 export default Events;
