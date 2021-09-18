2896266c45cd07d097aa928bc8638c08ac0afece
Sören Dänekas <daenekassoeren@gmail.com>
2021-09-18 23:29:59 +0200

    Add comment for future and make the form bigger

--- src/FormPage.js
+++ src/FormPage.js
@@ -7,7 +7,13 @@ export default function FormPage(props) {
         <div className="pageContainer">
             <div className="pt-5 container">
                 <div className="row justify-content-center"> 
-                    <div className="col-xl-6 col-lg-8 col-md-10 col-12 createAccount bg-light">  {/* todo: there must be a nicer way */}
+                {/* 
+                
+                todo: there must be a nicer way
+                Intended is that this autosizes 
+                
+                */}
+                    <div className="col-xl-7 col-lg-9 col-md-11 col-12 createAccount bg-light"> 
                         {props.children}
                     </div>
                 </div>
